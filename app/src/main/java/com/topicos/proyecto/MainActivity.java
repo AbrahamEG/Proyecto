32b33ad2b7f1c541ed78f16666a746ac54ac6f52
AbrahamEG <abrahameg94@gmail.com>
2019-04-24 13:57:33 -0500

    Commit

--- app/src/main/java/com/topicos/proyecto/MainActivity.java
+++ app/src/main/java/com/topicos/proyecto/MainActivity.java
@@ -1,7 +1,10 @@
 package com.topicos.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView t=(TextView)findViewById(R.id.reg);
+
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in= new Intent(MainActivity.this, Registro.class);
+                startActivity(in);
+            }
+        });
+
     }
 }
