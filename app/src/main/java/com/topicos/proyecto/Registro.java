e2bebaf020e6af8d2a3ac41ccd0db91cba2b2cb1
Jose Maria Gonzalez <gonzalezperezj5@gmail.com>
2019-04-26 15:57:41 -0500

    Commit

--- app/src/main/java/com/topicos/proyecto/Registro.java
+++ app/src/main/java/com/topicos/proyecto/Registro.java
@@ -1,13 +1,96 @@
 package com.topicos.proyecto;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Registro extends AppCompatActivity {
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+
+    EditText no, ap, am, co,cont,contC;
+    Button acep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        no = (EditText)findViewById(R.id.nomR);
+        ap = (EditText)findViewById(R.id.nomR);
+        am = (EditText)findViewById(R.id.nomR);
+        co = (EditText)findViewById(R.id.nomR);
+        cont = (EditText)findViewById(R.id.nomR);
+        contC = (EditText)findViewById(R.id.nomR);
+
+        acep = (Button) findViewById(R.id.btnAceR);
+        acep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                altaclase(v);
+            }
+        });
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+    public void altaclase(View view)
+    {
+        sqlLite admin = new sqlLite(this,"proyecto",null,1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+        String nom = no.getText().toString();
+        String app = ap.getText().toString();
+        String apm = am.getText().toString();
+        String cor = co.getText().toString();
+        String con = cont.getText().toString();
+        String con1 = contC.getText().toString();
+        ContentValues registro = new ContentValues();
+
+        if( nom.isEmpty() || app.isEmpty() || apm.isEmpty() ||  con.isEmpty() || con1.isEmpty() || cor.isEmpty())
+        {
+            Toast.makeText(this, "Hay campos incorrectos o vacios",Toast.LENGTH_SHORT).show();
+        }else if (!(con.equals(con1))){
+            Toast.makeText(this, "Las contraseñas deben ser iguales",Toast.LENGTH_SHORT).show();
+        }else if (!(cor.isEmpty())){
+            Cursor cursor = db.rawQuery("select id from usuario where id=" + cor , null);
+            if (cursor.moveToFirst()){
+                Toast.makeText(this, "El usuario: "+cor+" actualmente esta en uso",Toast.LENGTH_SHORT).show();
+            }else{
+                registro.put("id", cor);
+                registro.put("nombre", nom);
+                registro.put("app", app);
+                registro.put("apm", apm);
+                registro.put("correo", cor);
+                registro.put("contraseña", con);
+                db.insert("usuario", null, registro);
+                db.close();
+                AlertDialog.Builder builder= new AlertDialog.Builder(this);
+                builder.setCancelable(true);
+                builder.setTitle("Registro Exitoso");
+                builder.setMessage("\nTu usuario para ingresar al sistema es:\n"+cor);
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent ListFrutas = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(ListFrutas);
+                    }
+                });
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
+            }
+        }else{
+            Toast.makeText(this, "Campo: NUMERO DE CONTROL se encuentra vacio",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/topicos/proyecto/sqlLite.java
@@ -0,0 +1,25 @@
+package com.topicos.proyecto;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class sqlLite extends SQLiteOpenHelper {
+    public sqlLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table usuario(id text primary key, nombre text, app text, apm text, corre text, cont text, contC text)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists usuario");
+        db.execSQL("create table usuario (id text primary key, nombre text, app text, apm text, corre text, cont text, contC text)");
+
+
+    }
+}
